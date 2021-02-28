7f4be3670ea47cb2e6a08f353340fea0e19725e5
Andrew Kraynak <abk89@miami.edu>
2021-02-28 16:45:58 -0500

    Added methods to Lock class (get/set name, pin)

--- lock.py
+++ lock.py
@@ -1,4 +1,20 @@
 class Lock:
-    def __init__(self, name: str):
+    def __init__(self, name: str, pin: int):
         self._name = name
+        self._pin = pin
         self._enable = False
+
+    def get_name(self) -> str:
+        return self._name
+
+    def set_name(self, name: str):
+        self._name = name
+
+    def get_pin(self) -> int:
+        return self._pin
+
+    def set_pin(self, pin: int):
+        self._pin = pin
+
+    def get_status(self) -> bool:
+        return self._enable
