cdbc9f2aa0f97bb43a21f2ff613d43e7a2fccf74
Andrew Kraynak <abk89@miami.edu>
2021-02-28 17:00:10 -0500

    Reduced duplication of funcs & updated home-test

--- home-test.py
+++ home-test.py
@@ -3,29 +3,29 @@ from home import Home
 h0 = Home('Andrew', 'SW 12th St')
 print(h0._firstName)
 print(h0._address)
-print(h0.get_alarm_pin())
+print(h0._alarm.get_pin())
 
-h0.set_alarm_pin(1234)
-print(h0.get_alarm_pin())
+h0._alarm.set_pin(1234)
+print(h0._alarm.get_pin())
 
-h0.enable_alarm(4321)
-status = h0.get_alarm_status()
-print(status)
-
-h0.enable_alarm(1234)
+h0._alarm.enable(4321)
 status = h0._alarm.get_status()
 print(status)
 
-h0.enable_alarm(1234)
-status = h0.get_alarm_status()
+h0._alarm.enable(1234)
+status = h0._alarm.get_status()
 print(status)
 
-h0.disable_alarm(4321)
-status = h0.get_alarm_status()
+h0._alarm.enable(1234)
+status = h0._alarm.get_status()
 print(status)
 
-h0.disable_alarm(1234)
-status = h0.get_alarm_status()
+h0._alarm.disable(4321)
+status = h0._alarm.get_status()
+print(status)
+
+h0._alarm.disable(1234)
+status = h0._alarm.get_status()
 print(status)
 
 
--- home.py
+++ home.py
@@ -8,17 +8,4 @@ class Home(object):
         self._alarm = Alarm(0)
         self._locks = []
 
-    def get_alarm_pin(self) -> int:
-        return self._alarm.get_pin()
-
-    def set_alarm_pin(self, pin: int):
-        self._alarm.set_pin(pin)
-
-    def get_alarm_status(self) -> int:
-        return self._alarm.get_status()
-
-    def enable_alarm(self, pin: int):
-        self._alarm.enable(pin)
-
-    def disable_alarm(self, pin: int):
-        self._alarm.disable(pin)
+    
