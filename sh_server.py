35464b39d80a1b35675f2754a29bae6cca7758fb
Andrew Kraynak <abk89@miami.edu>
2021-03-02 15:04:27 -0500

    Added SHServer class

--- sh_server.py
+++ sh_server.py
@@ -1,1 +1,10 @@
 # sh_server.py
+
+from message import Message
+from sh_protocol import SHProtocol
+
+class SHServer(object):
+    def __init__(self, s: SHProtocol):
+        self._shp = s
+        
+    
