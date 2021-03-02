cc25533a1cf774e7f2b20fbce2f30be67ee4c725
Andrew Kraynak <abk89@miami.edu>
2021-03-02 14:39:06 -0500

    Added methods to SHProtocol

--- sh_protocol.py
+++ sh_protocol.py
@@ -9,6 +9,29 @@ class SHProtocol(object):
 
     def __init__(self, s: socket):
         self._sock = s
-        self._rfile = self._sock.makefile(mode='r', encooding='utf-8',newline=SHProtocol.CRLF)
+        self._rfile = self._sock.makefile(mode='r', encooding='utf-8', newline=SHProtocol.CRLF)
 
-    
+    def _receive_line(self) -> str:
+        s = self._rfile.readline()
+        return s
+
+    def put_message(self, message: Message):
+        data = message.marshal()
+        self._sock.sendall(data.encode('utf-8'))
+
+    def get_message(self) -> Message:
+        lines = []
+
+        # Get first 2 lines that contain type and parameter (lines)
+        lines.append(self._receive_line())
+        lines.append(self._receive_line())
+        m = Message()
+        m.unmarshal(''.join(lines))
+        count = int(m.getParam('lines'))
+
+        # Get the remaining lines of the message (body)
+        for i in range(count):
+            m.addLine(self._receive_line())
+
+        return m
+
