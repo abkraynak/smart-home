1829030e7bc06374307ec7303bda9a321eebf2f2
Andrew Kraynak <abk89@miami.edu>
2021-03-02 15:43:13 -0500

    Updated tcp_server with sh_protocol

--- sh_client.py
+++ sh_client.py
@@ -1,1 +1,5 @@
 # sh_client.py
+
+from message import Message
+from sh_protocol import SHProtocol
+
--- tcp_server.py
+++ tcp_server.py
@@ -1,52 +1,35 @@
 # tcp_server.py
 
 import socket
-
-def loopRecv(csoc, size):
-    data = bytearray(b" "*size)
-    mv = memoryview(data)
-    while size:
-        rsize = csoc.recv_into(mv,size)
-        mv = mv[rsize:]
-        size -= rsize
-    return data
-
-def baseTCPProtocolS(csoc):
-    print("Started baseTCPProtocol")
-    
-    # recv 10 bytes from the client
-    data = loopRecv(csoc,10)
-    
-    # send 10 bytes to client
-    csoc.sendall(data)
-    
-    print("Ended baseTCPProtocol")
+from sh_protocol import SHProtocol
+from sh_server import SHServer
     
 if __name__ == "__main__":
-    # create the server socket
-    #  defaults family=AF_INET, type=SOCK_STREAM, proto=0, filno=None
+    # Create the server socket
+    # Defaults family=AF_INET, type=SOCK_STREAM, proto=0, filno=None
     serversoc = socket.socket()
     
-    # bind to local host:5000
-    serversoc.bind(("localhost",5000))
+    # Bind to local host:50000
+    serversoc.bind(("localhost",50000))
                    
-    # make passive with backlog=5
+    # Make passive with backlog=5
     serversoc.listen(5)
     
-    # wait for incoming connections
+    # Wait for incoming connections
     while True:
-        print("Listening on ", 5000)
+        print("Listening on ", 50000)
         
-        # accept the connection
+        # Accept the connection
         commsoc, raddr = serversoc.accept()
         
-        # run the application protocol
-        baseTCPProtocolS(commsoc)
+        # Run the application protocol
+        shp = SHProtocol(commsoc)
+        shs = SHServer(shp)
+        shs.run()
         
-        
-        # close the comm socket
+        # Close the comm socket
         commsoc.close()
     
-    # close the server socket
+    # Close the server socket
     serversoc.close()
     
