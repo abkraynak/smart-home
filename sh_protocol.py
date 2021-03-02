f4acc5205c4c384771e2aa77c402518d413cb2e4
Andrew Kraynak <abk89@miami.edu>
2021-03-02 14:26:40 -0500

    Started SHProtocol class

--- sh_protocol.py
+++ sh_protocol.py
@@ -1,1 +1,14 @@
 # sh_protocol.py
+
+import socket
+from message import Message
+
+class SHProtocol(object):
+    CRLF = '\r\n'
+    BUFSIZE = 8196
+
+    def __init__(self, s: socket):
+        self._sock = s
+        self._rfile = self._sock.makefile(mode='r', encooding='utf-8',newline=SHProtocol.CRLF)
+
+    
