fa3872c3ac30814bf2b4233077f9f36802fab811
Andrew Kraynak <abk89@miami.edu>
2021-03-02 15:10:58 -0500

    Added run method to SHServer

--- sh_server.py
+++ sh_server.py
@@ -7,4 +7,15 @@ class SHServer(object):
     def __init__(self, s: SHProtocol):
         self._shp = s
         
-    
+    def run(self):
+        m0_recv = self._shp.get_message()
+        print(m0_recv)
+
+        # First message sent is username request
+        m1_send = Message()
+        m1_send.setType('USER')
+        m1_send.addParam('user', 'none')
+        m1_send.addLine('Enter username:')
+        self._shp.put_message(m1_send)
+
+        
