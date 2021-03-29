778b01997cf7a208ea1f71a92be70efc7896f659
Andrew Kraynak <abk89@miami.edu>
2021-03-29 16:11:27 -0400

    Fixed comments

--- tcp_client.py
+++ tcp_client.py
@@ -9,10 +9,10 @@ PORT = 50000
 
 if __name__ == "__main__":
     # Create the socket
-    # Defaults family=AF_INET, type=SOCK_STREAM, proto=0, filno=None
+    # Defaults: family=AF_INET, type=SOCK_STREAM, proto=0, filno=None
     commsoc = socket.socket()
     
-    # Connect to localhost:50001
+    # Connect to localhost:50000
     commsoc.connect((HOST, PORT))
     
     # Run the application protocol
@@ -20,5 +20,5 @@ if __name__ == "__main__":
     shc = SHClient(shp)
     shc.run()
     
-    # Close the comm socket
+    # Close the socket
     commsoc.close()
